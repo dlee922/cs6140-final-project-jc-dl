6f967ee7f217572fb2033b732c47fb17fc833aba
Jason Chen <jianxinchen298@gmail.com>
2026-04-11 02:07:37 -0400

    evaluates models saved in models/fitted_models/

---
+++ scripts/evaluate.py
@@ -0,0 +1,68 @@
+from sklearn.metrics import accuracy_score, f1_score
+from utils.model_utils import load_train_test
+from pathlib import Path
+import argparse
+import joblib
+import pandas as pd
+
+feature_set_paths = {
+    'clinical': 'data/processed/X_clinical.csv',
+    'genomic': 'data/processed/X_genomic.csv',
+    'combined': 'data/processed/X_combined.csv'
+}
+
+def main():
+
+    args = get_cli_args()
+    y_filepath = "data/processed/y.csv"
+    X_filepath = feature_set_paths[args.feature_set]
+    X_train, X_test, y_train, y_test = load_train_test(X_filepath, y_filepath)
+
+    # load
+    path = Path('models/fitted_models')
+    models = {}
+    for item in path.iterdir():
+        if item.is_file():
+            model = joblib.load(str(item))
+            model_name = str(item).split('/')[-1].replace(f'_{args.feature_set}.pkl', '')
+            models[model_name] = model
+            
+    # evaluate
+    eval_results = {}
+    for model_name, model in models.items():
+        eval_results[model_name] = evaluate_model(model, (X_train, X_test, y_train, y_test))
+
+    # save results
+    df_eval = pd.DataFrame(eval_results)
+    df_eval.to_csv(f"results/evaluation_{args.feature_set}.csv") 
+
+def evaluate_model(model, data: tuple) -> dict:
+    X_train, X_test, y_train, y_test = data
+    y_pred_test = model.predict(X_test)
+    y_pred_train = model.predict(X_train)
+
+    return {
+        'test_f1': f1_score(y_test, y_pred_test, average='macro', zero_division=0),
+        'test_accuracy': accuracy_score(y_test, y_pred_test),
+        'train_f1': f1_score(y_train, y_pred_train, average='macro', zero_division=0),
+        'train_accuracy': accuracy_score(y_train, y_pred_train)
+    }
+
+
+def get_cli_args():
+    parser = argparse.ArgumentParser(description='Description')
+    parser.add_argument('--feature_set',
+                        '-f',
+                        type=str,
+                        help='select clinical, genomic, or combined',
+                        required=True)
+    parser.add_argument('--model',
+                        '-m',
+                        type=str,
+                        help='select which model to run',
+                        required=False,
+                        default='All')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
